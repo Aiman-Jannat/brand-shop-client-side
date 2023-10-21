dc960a173fa5f8b9cbecbe687daa58933e2cc94e
Aiman-Jannat <ummeaimanjannat@gmail.com>
2023-10-21 08:49:54 +0600

    delete complete

--- src/components/Pages/MyCart.jsx
+++ src/components/Pages/MyCart.jsx
@@ -1,18 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
+import ShowMyCarts from "./ShowMyCarts";
+let chooseCarts=[];
 
 
 const MyCart = () => {
 
     const allMyCarts = useLoaderData();
     const {userr} = useContext(AuthContext);
-    const array = allMyCarts.filter(data => data.email==userr.email);
-    console.log(array);
+    
+    if(userr){
+    console.log(userr.email)
+    chooseCarts = allMyCarts.filter(data => data.email==(userr.email));
+    
+    
+}
+     
     return (
-        <div>
-            
+        <>
+        <p className="my-12 p-5 w-fit mx-auto text-center text-4xl text-pink-900 font-bold border-2 border-red-950">All My Selected Carts</p>
+        <div className="p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {
+                chooseCarts.map(show =><ShowMyCarts key={show._id} show={show}></ShowMyCarts> )
+            }
         </div>
+        </>
     );
 };
 
---
+++ src/components/Pages/ShowMyCarts.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+
+const ShowMyCarts = ({show,}) => {
+    const {_id, name, brandName, rating, type,price
+        ,photo,description} = show;
+        
+
+        
+   
+        const handleDel = _id =>{
+            fetch(`http://localhost:5000/carts/${_id}`,{
+                method:'DELETE'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+            })
+        }
+
+
+    return (
+        <div>
+            <div className="flex flex-grow">
+          <div className="flex flex-grow">
+            <div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="h-52"><img className="h-full w-full" src={photo} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      {name}
+      <div className="badge badge-secondary">{type}</div>
+    </h2>
+    <p>{description}</p>
+    <div className="block">
+      <div className="badge badge-outline">{price}</div> 
+      <div className="badge badge-outline">{brandName}</div>
+      </div>
+    <div className="card-actions justify-end">
+    <div className="rating flex justify-start  w-full">
+  <input type="radio" name={`rating-${rating}`} className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name={`rating-${rating}`}  className="mask mask-star-2 bg-orange-400" checked />
+  <input type="radio" name={`rating-${rating}`}  className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name={`rating-${rating}`}  className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name={`rating-${rating}`}  className="mask mask-star-2 bg-orange-400" />
+</div>
+</div>
+<div className=" w-full mt-5 gap-5 flex justify-around">
+<Link><button onClick={()=>handleDel(_id)} className="btn px-14 text-white font-bold btn-error w-full">Delete</button></Link>
+
+    
+</div>
+
+
+    </div>
+  </div>
+</div>
+        </div>
+        </div>
+    );
+};
+
+export default ShowMyCarts;
